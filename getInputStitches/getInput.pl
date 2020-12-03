7818de9bc5c3d86edbbdd7ed281b5387f54f02c2
Squame4 <55781259+Squame4@users.noreply.github.com>
2020-12-03 21:04:47 +0100

    Add files via upload
    Upload dello script per prendere gli input

---
+++ getInputStitches/getInput.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/perl
+
+$fileInput = shift || die "Nessun file di input fornito";
+
+die "Troppi parametri dati in input" if (scalar @ARGV);
+
+$sumsString;
+$groupsInput;
+
+@input = qx{cat $fileInput};
+
+for(@input){
+    
+    chomp;
+
+    if($_ =~ m/"(.+);(.+)"/){
+        $sumsString = $1;
+        $groupsInput = $2;
+    }
+
+}
+
+@sums = split('_', $sumsString);
+
+$size = scalar @sums;
+$n = $size/2;
+$i = 1;
+#print($i);
+
+@rowSums;
+@colSums;
+
+for(@sums){
+
+    if($i <= $size/2){
+        push(@colSums, $_);
+    }
+    else{
+        push(@rowSums, $_);
+    }
+
+    $i++;
+
+}
+
+
+@groups = split(',', $groupsInput);
+
+open($data, ">", "data.dzn") || die "Impossibile aprire il file $! \n";
+
+print $data "n = $n;\n";
+
+$i = $n;
+print $data "colSum = [";
+for(@colSums){
+    
+    print $data "$_";
+    $i--;
+    if ($i > 0){
+        print $data ",";
+    }
+
+}
+print $data "];\n";
+
+$i = $n;
+print $data "rowSum = [";
+for(@rowSums){
+    
+    print $data "$_";
+    $i--;
+    if ($i > 0){
+        print $data ",";
+    }
+
+}
+print $data "];\n";
+
+print $data "groups =\n";
+print $data "[";
+$i = $n;
+
+for(@groups){
+
+    if($i == $n){
+        print $data "| ";
+    }
+
+    print $data "$_";
+
+    $i--;
+    if ($i > 0){
+        print $data ",";
+    }
+    else{
+        print $data "\n";
+        $i = $n;
+    }
+
+}
+
+print $data "|];";
+
+close $data;
